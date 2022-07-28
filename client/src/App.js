4f76ca884f557cd080c9913dafa396deb0a3f32b
Dimos <dimospagkas@yahoo.gr>
2022-07-28 12:31:04 -0400

    create the schemas

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,36 @@
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-
-import { ApolloProvider } from "@apollo/react-hooks"
-import ApolloClient from "apollo-boost"
-
-import Navbar from "./components/Navbar"
-import SavedBooks from "./pages/SavedBooks"
-import SearchBooks from "./pages/SearchBooks"
-
-// establish apollo client
-const client = new ApolloClient({
-    request: operation => {
-        const token = localStorage.getItem("id_token")
-
-        operation.setContext({
-            headers: {
-                authorization: token ? `Bearer ${token}` : "",
-            },
-        })
-    },
+import { BrowserRouter as Router } from "react-router-dom"
+// import Home from './pages/Home';
+const httpLink = createHttpLink({
     uri: "/graphql",
 })
-
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem("id_token")
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        },
+    }
+})
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+})
 function App() {
     return (
         <ApolloProvider client={client}>
             <Router>
-                <>
-                    <Navbar />
-                    <Switch>
-                        <Route exact path="/" component={SearchBooks} />
-                        <Route exact path="/saved" component={SavedBooks} />
-                        <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-                    </Switch>
-                </>
+                {/* <div> */}
+                {/* <Nav />
+          <Switch>
+            {/* <Route exact path="/" component={Home} /> */}
+                {/* </Switch> */}
+                {/* </div> */}
             </Router>
         </ApolloProvider>
     )
 }
-
 export default App
