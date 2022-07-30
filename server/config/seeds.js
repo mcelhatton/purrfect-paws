01b05701fdd2157527df54bfeef7b2cc9781be4c
Dimos <dimospagkas@yahoo.gr>
2022-07-29 20:21:24 -0400

    update code to reduce empty lines

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,101 +1,97 @@
-const db = require('./connection');
-const { User, Product, Category } = require('../models');
+const db = require("./connection")
+const { User, Product, Category } = require("../models")
 
-db.once('open', async () => {
-  await Category.deleteMany();
+db.once("open", async () => {
+    await Category.deleteMany()
 
-  const categories = await Category.insertMany([
-    { name: 'Cats' },
-    { name: 'Dogs' },
-    { name: 'Turtles' },
-    { name: 'Lamas' }
-  ]);
+    const categories = await Category.insertMany([
+        { name: "Cats" },
+        { name: "Dogs" },
+        { name: "Turtles" },
+        { name: "Lamas" },
+    ])
 
-  console.log('categories seeded');
+    console.log("categories seeded")
 
-  await Product.deleteMany();
+    await Product.deleteMany()
 
-  const products = await Product.insertMany([
-    {
-      name: 'Cat head scratching 1 hour',
-      description:
-        'Give your cat the luxury it demands with a one hour head scratching service from Purrfect Paws!',
-      image: 'kitty.jpg',
-      category: categories[0]._id,
-      price: 99.99,
-      quantity: 1
-    },
-    {
-      name: 'Cat hair shaving',
-      description:
-        'Are you upset with your cat?  Did it wake you up at 3am?  Pay your cat back with our Full Cat Shaving service!',
-      image: 'kitty1.jpg',
-      category: categories[0]._id,
-      price: 1.99,
-      quantity: 1
-    },
-    {
-      name: 'Dog Hair Cutt',
-      category: categories[1]._id,
-      description:
-        'Is your dog shedding everywhere?  Get your dog a Purrfect Haircut!',
-      image: 'puppy.jpg',
-      price: 37.99,
-      quantity: 20
-    },
-    {
-      name: 'Turtle Wax',
-      category: categories[1]._id,
-      description:
-        'Make your turtle Purr with a shinny shell waxing!.',
-      image: 'turtle.jpg',
-      price: 33.99,
-      quantity: 1
-    },
-    {
-      name: 'Nail clipping ',
-      category: categories[1]._id,
-      description:
-        'Just like us dogs like a fresh nail clipping!',
-      image: 'puppy1.jpg',
-      price: 24.99,
-      quantity: 1
-    },
-    {
-      name: 'Camel Camp',
-      category: categories[2]._id,
-      description:
-        'Everyone knows Camels are very social.  Sign your camel up for our Camel Camp!',
-      image: 'camel.jpg',
-      price: 399.99,
-      quantity: 30
-    }
-  ]);
+    const products = await Product.insertMany([
+        {
+            name: "Cat head scratching 1 hour",
+            description:
+                "Give your cat the luxury it demands with a one hour head scratching service from Purrfect Paws!",
+            image: "kitty.jpg",
+            category: categories[0]._id,
+            price: 99.99,
+            quantity: 1,
+        },
+        {
+            name: "Cat hair shaving",
+            description:
+                "Are you upset with your cat?  Did it wake you up at 3am?  Pay your cat back with our Full Cat Shaving service!",
+            image: "kitty1.jpg",
+            category: categories[0]._id,
+            price: 1.99,
+            quantity: 1,
+        },
+        {
+            name: "Dog Hair Cutt",
+            category: categories[1]._id,
+            description: "Is your dog shedding everywhere?  Get your dog a Purrfect Haircut!",
+            image: "puppy.jpg",
+            price: 37.99,
+            quantity: 20,
+        },
+        {
+            name: "Turtle Wax",
+            category: categories[1]._id,
+            description: "Make your turtle Purr with a shinny shell waxing!.",
+            image: "turtle.jpg",
+            price: 33.99,
+            quantity: 1,
+        },
+        {
+            name: "Nail clipping ",
+            category: categories[1]._id,
+            description: "Just like us dogs like a fresh nail clipping!",
+            image: "puppy1.jpg",
+            price: 24.99,
+            quantity: 1,
+        },
+        {
+            name: "Camel Camp",
+            category: categories[2]._id,
+            description: "Everyone knows Camels are very social.  Sign your camel up for our Camel Camp!",
+            image: "camel.jpg",
+            price: 399.99,
+            quantity: 30,
+        },
+    ])
 
-  console.log('products seeded');
+    console.log("products seeded")
 
-  await User.deleteMany();
+    await User.deleteMany()
 
-  await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
-    password: 'password12345',
-    orders: [
-      {
-        products: [products[0]._id, products[0]._id, products[1]._id]
-      }
-    ]
-  });
+    await User.create({
+        firstName: "Pamela",
+        lastName: "Washington",
+        email: "pamela@testmail.com",
+        password: "password12345",
+        orders: [
+            {
+                products: [products[0]._id, products[0]._id, products[1]._id],
+            },
+        ],
+    })
 
-  await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
-    password: 'password12345'
-  });
+    await User.create({
+        firstName: "Elijah",
+        lastName: "Holt",
+        email: "eholt@testmail.com",
+        password: "password12345",
+    })
 
-  console.log('users seeded');
+    console.log("users seeded")
 
-  process.exit();
-});
+    process.exit()
+})
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,71 +1,61 @@
-const { gql } = require("apollo-server-express");
+const { gql } = require("apollo-server-express")
 
 const typeDefs = gql`
-  type Category {
-    _id: ID
-    name: String
-  }
+    type Category {
+        _id: ID
+        name: String
+    }
 
-  type Product {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    quantity: Int
-    price: Float
-    category: Category
-  }
+    type Product {
+        _id: ID
+        name: String
+        description: String
+        image: String
+        quantity: Int
+        price: Float
+        category: Category
+    }
 
-  type Checkout {
-    session: ID
-  }
+    type Checkout {
+        session: ID
+    }
 
-  type Order {
-    _id: ID
-    purchaseDate: String
-    products: [Product]
-  }
+    type Order {
+        _id: ID
+        purchaseDate: String
+        products: [Product]
+    }
 
-  type User {
-    _id: ID
-    firstName: String
-    lastName: String
-    email: String
-    orders: [Order]
-  }
+    type User {
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+        orders: [Order]
+    }
 
-  type Auth {
-    token: ID
-    user: User
-  }
+    type Auth {
+        token: ID
+        user: User
+    }
 
-  type Query {
-    users: [User]
-    categories: [Category]
-    products(category: ID, name: String): [Product]
-    product(_id: ID!): Product
-    user: User
-    order(_id: ID!): Order
-    checkout(products: [ID]!): Checkout
-  }
+    type Query {
+        users: [User]
+        categories: [Category]
+        products(category: ID, name: String): [Product]
+        product(_id: ID!): Product
+        user: User
+        order(_id: ID!): Order
+        checkout(products: [ID]!): Checkout
+    }
 
-  type Mutation {
-    addUser(
-      firstName: String!
-      lastName: String!
-      email: String!
-      password: String!
-    ): Auth
-    addOrder(products: [ID]!): Order
-    updateUser(
-      firstName: String
-      lastName: String
-      email: String
-      password: String
-    ): User
-    updateProduct(_id: ID!, quantity: Int!): Product
-    login(email: String!, password: String!): Auth
-  }
-`;
+    type Mutation {
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        addOrder(products: [ID]!): Order
+        updateUser(firstName: String, lastName: String, email: String, password: String): User
+        updateProduct(_id: ID!, quantity: Int!): Product
+        login(email: String!, password: String!): Auth
+    }
+`
 
-module.exports = typeDefs;
+module.exports = typeDefs
