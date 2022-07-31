cc2f3eb91558de05c1e60aa9dbbf85cfa57350d5
Dimos <dimospagkas@yahoo.gr>
2022-07-31 16:03:35 -0400

    update footer disable carousel

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,10 +3,15 @@ import { Grid, List } from "semantic-ui-react"
 
 const Footer = () => (
     <div className="footer">
-        <Grid.Column width={3}>
+        <Grid divided inverted stackable>
             <a id="footext2">Developers</a>
-            <List link inverted>
-                <List.Item href="https://github.com/djpdim" target="_blank" rel="noopener noreferrer">
+            <List link inverted className="githubnames">
+                <List.Item
+                    href="https://github.com/djpdim"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    // style={{ color: "red" }}
+                >
                     djpdim
                 </List.Item>
                 <List.Item href="https://github.com/rpliszka1987" target="_blank" rel="noopener noreferrer">
@@ -28,36 +33,39 @@ const Footer = () => (
 
             <p id="footext">
                 &copy; 2022 <a> </a>
-                {/* <a href="https://github.com/djpdim" target="_blank" rel="noopener noreferrer">
-                    djpdim
-                </a>
-                <a> </a>
-                <a id="acolor"> | </a>
-                <a href="https://github.com/rpliszka1987" target="_blank" rel="noopener noreferrer">
-                    rpliszka1987
-                </a>
-                <a> </a>
-                <a id="acolor"> | </a>
-                <a href="https://github.com/vmedina927" target="_blank" rel="noopener noreferrer">
-                    vmedina927
-                </a>
-                <a> </a>
-                <a id="acolor"> | </a>
-                <a href="https://github.com/mcelhatton" target="_blank" rel="noopener noreferrer">
-                    mcelhatton
-                </a>
-                <a> </a>
-                <a id="acolor"> | </a>
-                <a href="https://github.com/cpriyam90" target="_blank" rel="noopener noreferrer">
-                    cpriyam90
-                </a>
-                <a> </a>
-                <a id="acolor"> | </a>
-                <a href="https://github.com/MaameSekyere" target="_blank" rel="noopener noreferrer">
-                    MaameSekyere
-                </a> */}
             </p>
-        </Grid.Column>
+
+            <a id="footext2">Developers</a>
+            <List link inverted className="githubnames">
+                <List.Item
+                    href="https://github.com/djpdim"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    // style={{ color: "red" }}
+                >
+                    djpdim
+                </List.Item>
+                <List.Item href="https://github.com/rpliszka1987" target="_blank" rel="noopener noreferrer">
+                    rpliszka1987
+                </List.Item>
+                <List.Item href="https://github.com/vmedina927" target="_blank" rel="noopener noreferrer">
+                    vmedina927
+                </List.Item>
+                <List.Item href="https://github.com/mcelhatton" target="_blank" rel="noopener noreferrer">
+                    mcelhatton
+                </List.Item>
+                <List.Item href="https://github.com/cpriyam90" target="_blank" rel="noopener noreferrer">
+                    cpriyam90
+                </List.Item>
+                <List.Item href="https://github.com/MaameSekyere" target="_blank" rel="noopener noreferrer">
+                    MaameSekyere
+                </List.Item>
+            </List>
+
+            <p id="footext">
+                &copy; 2022 <a> </a>
+            </p>
+        </Grid>
     </div>
 )
 
